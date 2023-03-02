8e091b0aff31b3a97516fc7fc7bbac08c84ddcc9
rodrigojmayer <rodrigojmayer>
2023-03-02 10:36:22 +0100

    cors

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
+var cors = require('cors')
 
 // express app
 const app = express()
 
+app.use(cors())
 // middleware
 app.use(express.json())
 
